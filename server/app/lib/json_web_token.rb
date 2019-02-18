74afbfacb43922cc90134469f6186ada46798766
lap14tclc2 <honaz140@gmail.com>
2019-02-18 20:53:04 +0700

    add helper method Message

--- server/app/lib/json_web_token.rb
+++ server/app/lib/json_web_token.rb
@@ -1,6 +1,6 @@
 class JsonWebToken
   # secret key to encode and decode token
-  HMAC_SECRET = JOqkv9k82m3qfKOhgDQgtiGlTP4KUEt3cK6/HjJ0fEuePI7YEuQpbnI43WIWemZnnP+8wKmkJFl5PrIcBThD+6hmYzVcT6lToSJHSZKy7IIq3dyU3uzQZqJ6HTSWqVLAvA70VPF0+dd4jof/HmPj0/UrA/brC4E291J0Sg6pMPuiSuLjPxYCOMeVOSQ8ucKLwXptLgPaFbUy4dgTA/zjsHVIFgOa2zbXs3mgv+sbBnSvidGpCfh5s4rnLVFL0GM+ijmEgWmd4yLGftYfWbuVUuMzGWPzkOj2j7v6osgzVT6LmZrcQZvbybk9CizeyGuurLjXWJI36joNPtVHFcgReryihDipqoEIlKhJ4jhmZQyaiPs6kQXT+PxljAJ6LvPQ/HUxRb2YKlADLlxMmokhvYK+X8R/fF4RxyKA--2YKJ1bImBR23L5Ns--pJPAKHLbQ6jdmIFPNWzxMw==  
+  HMAC_SECRET = "JOqkv9k82m3qfKOhgDQgtiGlTP4KUEt3cK6/HjJ0fEuePI7YEuQpbnI43WIWemZnnP+8wKmkJFl5PrIcBThD+6hmYzVcT6lToSJHSZKy7IIq3dyU3uzQZqJ6HTSWqVLAvA70VPF0+dd4jof/HmPj0/UrA/brC4E291J0Sg6pMPuiSuLjPxYCOMeVOSQ8ucKLwXptLgPaFbUy4dgTA/zjsHVIFgOa2zbXs3mgv+sbBnSvidGpCfh5s4rnLVFL0GM+ijmEgWmd4yLGftYfWbuVUuMzGWPzkOj2j7v6osgzVT6LmZrcQZvbybk9CizeyGuurLjXWJI36joNPtVHFcgReryihDipqoEIlKhJ4jhmZQyaiPs6kQXT+PxljAJ6LvPQ/HUxRb2YKlADLlxMmokhvYK+X8R/fF4RxyKA--2YKJ1bImBR23L5Ns--pJPAKHLbQ6jdmIFPNWzxMw=="
   
   def self.encode(payload, exp = 24.hours.from_now)
     # set expiry to 24h from now
