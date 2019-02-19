e35fd4bb8234d658a32b45dd13614c5d69cfd5c1
quynguyen1510 <quycao1510@gmail.com>
2019-02-19 09:00:26 +0700

    Add field avatar for user

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_131323) do
+ActiveRecord::Schema.define(version: 2019_02_19_015900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_02_18_131323) do
     t.bigint "role_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
